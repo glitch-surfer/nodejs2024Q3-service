bf0a6d61b57522b2a93d5d094a9c69849ce7d56f
vladislav-garaev <marker661@gmail.com>
2024-11-09 21:17:26 +0400

    feat: implement users entity

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,19 @@
+import uuid from 'uuid';
+
+interface IUser {
+  id: string; // uuid v4
+  login: string;
+  password: string;
+  version: number; // integer number, increments on update
+  createdAt: number; // timestamp of creation
+  updatedAt: number; // timestamp of last update
+}
+
+export class User implements IUser {
+  id = uuid.v4();
+  version = 1;
+  createdAt = Date.now();
+  updatedAt = Date.now();
+
+  constructor(public login: string, public password: string) {}
+}
