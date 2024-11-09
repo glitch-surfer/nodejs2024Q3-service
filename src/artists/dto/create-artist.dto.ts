dc5ee6f1c9895744e99e066ae99f6b72802e377f
vladislav-garaev <marker661@gmail.com>
2024-11-10 01:14:10 +0400

    feat: add artists models

--- src/artists/dto/create-artist.dto.ts
+++ src/artists/dto/create-artist.dto.ts
@@ -1,1 +1,14 @@
-export class CreateArtistDto {}
+import { IsBoolean, IsNotEmpty, IsString } from 'class-validator';
+import { Artist } from '../entities/artist.entity';
+
+type ICreateArtistDto = Omit<Artist, 'id'>;
+
+export class CreateArtistDto implements ICreateArtistDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsBoolean()
+  @IsNotEmpty()
+  grammy: boolean;
+}
--- src/artists/entities/artist.entity.ts
+++ src/artists/entities/artist.entity.ts
@@ -1,1 +1,13 @@
-export class Artist {}
+import * as uuid from 'uuid';
+
+interface IArtist {
+  id: string; // uuid v4
+  name: string;
+  grammy: boolean;
+}
+
+export class Artist implements IArtist {
+  id = uuid.v4();
+
+  constructor(public name: string, public grammy: boolean) {}
+}
