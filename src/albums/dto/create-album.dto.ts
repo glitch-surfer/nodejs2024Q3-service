354c4941a0a7d0822dc54750cd47006acaff6460
vladislav-garaev <marker661@gmail.com>
2024-11-10 03:05:49 +0400

    feat: add album models

--- src/albums/dto/create-album.dto.ts
+++ src/albums/dto/create-album.dto.ts
@@ -1,1 +1,17 @@
-export class CreateAlbumDto {}
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+import { Album } from '../entities/album.entity';
+
+type ICreateAlbumDto = Omit<Album, 'id'>;
+
+export class CreateAlbumDto implements ICreateAlbumDto {
+  @IsNumber()
+  @IsNotEmpty()
+  year: number;
+
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  artistId: string;
+}
--- src/albums/entities/album.entity.ts
+++ src/albums/entities/album.entity.ts
@@ -1,1 +1,18 @@
-export class Album {}
+import * as uuid from 'uuid';
+
+interface IAlbum {
+  id: string; // uuid v4
+  name: string;
+  year: number;
+  artistId: string | null; // refers to Artist
+}
+
+export class Album implements IAlbum {
+  id = uuid.v4();
+
+  constructor(
+    public name: string,
+    public year: number,
+    public artistId: string,
+  ) {}
+}
