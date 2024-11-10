442328da8d4aac1dbff7d35ab47d1b97ea1b8cfc
vladislav-garaev <marker661@gmail.com>
2024-11-10 06:43:03 +0300

    feat: add track models

--- src/track/dto/create-track.dto.ts
+++ src/track/dto/create-track.dto.ts
@@ -1,1 +1,28 @@
-export class CreateTrackDto {}
+import {
+  IsNumber,
+  IsNotEmpty,
+  IsString,
+  IsUUID,
+  IsOptional,
+} from 'class-validator';
+import { Track } from '../entities/track.entity';
+
+type ICreateTrackDto = Omit<Track, 'id'>;
+
+export class CreateTrackDto implements ICreateTrackDto {
+  @IsNumber()
+  @IsNotEmpty()
+  duration: number;
+
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsUUID()
+  @IsOptional()
+  artistId: string | null;
+
+  @IsUUID()
+  @IsOptional()
+  albumId: string | null;
+}
--- src/track/entities/track.entity.ts
+++ src/track/entities/track.entity.ts
@@ -1,1 +1,20 @@
-export class Track {}
+import * as uuid from 'uuid';
+
+interface ITrack {
+  id: string; // uuid v4
+  name: string;
+  artistId: string | null; // refers to Artist
+  albumId: string | null; // refers to Album
+  duration: number; // integer number
+}
+
+export class Track implements ITrack {
+  id = uuid.v4();
+
+  constructor(
+    public name: string,
+    public artistId: string | null,
+    public albumId: string | null,
+    public duration: number,
+  ) {}
+}
