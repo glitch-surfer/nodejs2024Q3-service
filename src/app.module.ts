eb3bc4e0b47e2a4a68be6480e2aa32a5602ba867
vladislav-garaev <marker661@gmail.com>
2024-11-10 06:35:59 +0300

    feat: add raw track resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { UsersModule } from './users/users.module';
 import { ArtistsModule } from './artists/artists.module';
 import { ValidateUuidPipe } from './pipes/validate-uuid';
 import { AlbumsModule } from './albums/albums.module';
+import { TrackModule } from './track/track.module';
 
 @Module({
-  imports: [UsersModule, ArtistsModule, AlbumsModule],
+  imports: [UsersModule, ArtistsModule, AlbumsModule, TrackModule],
   controllers: [AppController],
   providers: [AppService, ValidateUuidPipe],
 })
